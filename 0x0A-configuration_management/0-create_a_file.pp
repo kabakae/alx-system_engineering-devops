7f374abbf1a755d981c45fbc08afaf4291a70ad6
kabakae <kabakaevans@gmail.com>
2024-04-19 23:09:43 -0700

    0000

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,2 @@
 # create a file
-file {'/tmp/school':
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I love Puppet'
-}
+file {'/tmp/school': mode =>'0744', owner =>'www-data', group => 'www-data', content => 'I love Puppet'}
