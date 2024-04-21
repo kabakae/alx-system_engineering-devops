0e3d317cd6dc282154cf6dbb118f19ba50387369
kabakae <kabakaevans@gmail.com>
2024-04-21 12:47:36 -0700

    0000

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Using puppet to connect without password
+
+file { '/etc/ssh/ssh_config':
+   ensure => present,
+}
+
+file_line { 'Turn off password auth':
+  path	=> '/etc/ssh/ssh_config',
+  line	=> 'PasswordAUthentication no',
+  match	=> '^#passwordAuthentication',
+}
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'identifyfile ~/.ssh/school',
+  match => '^#IdentifyFile',
+}
