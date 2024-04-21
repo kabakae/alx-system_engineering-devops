3917f61b64ace76b838834738d64f63b746fe1ff
kabakae <kabakaevans@gmail.com>
2024-04-21 12:57:02 -0700

    000

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,7 +5,7 @@ file { '/etc/ssh/ssh_config':
    ensure => present,
 }
 
-file_line { 'Turn off password auth':
+file_line { 'Turn off passwd auth':
   path	=> '/etc/ssh/ssh_config',
   line	=> 'PasswordAUthentication no',
   match	=> '^#passwordAuthentication',
