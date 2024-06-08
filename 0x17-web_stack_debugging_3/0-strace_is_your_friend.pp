2448e61d2ddea4dc0f1a555dfa8eac8649f3d157
kabakae <evanskabaka@gmail.com>
2024-06-08 17:14:04 +0000

    00

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,23 +1,6 @@
-# Puppet manifest to fix WordPress
-# This manifest applies fixes to the WordPress installation
+# Fixe bad "phpp" extensions to "php" in "wp-settings.php".
 
-# Executing the fix-wordpress command
-exec { 'fix-wordpress':
-  command     => '/path/to/fix-wordpress.sh',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  logoutput   => true,
-  refreshonly => true,
-}
-
-# Notify messages to indicate the status
-notify { 'Compiled catalog':
-  message => 'Compiled catalog for e514b399d69d.ec2.internal in environment production in 0.02 seconds';
-}
-
-notify { 'Exec[fix-wordpress]':
-  message => 'Exec[fix-wordpress] returned: executed successfully';
-}
-
-notify { 'Finished catalog run':
-  message => 'Finished catalog run in 0.08 seconds';
+exec{'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:bin'
 }
